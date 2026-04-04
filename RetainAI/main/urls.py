6ee3c959e282ff62f34fa8f7f712183246aa1d75
Ali Hussein <ah0979075074@gmail.com>
2026-04-04 13:43:07 +0300

    add submit_compliant

--- RetainAI/main/urls.py
+++ RetainAI/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.landing, name="landing"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("run-campaign/", views.stream_campaign, name="stream_campaign"),
+    path("submit-complaint/", views.get_complaint, name="get_complaint"),
 ]
